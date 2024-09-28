fd1dc846dbb00c3dff6641908ea64f7d61505043
Wizinoa <157701129+Wizinoa@users.noreply.github.com>
2024-09-28 15:00:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // When the request starts with /api, it will be proxied to the target server
+      '/api': {
+        target: 'http://65.0.93.147:4050', // The target backend server
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''), // Optional: remove /api from the request path
+      },
+      // You can add more proxies here if needed
+      // '/another-path': {
+      //   target: 'http://another-server.com',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/another-path/, ''),
+      // },
+    },
+  }
 })
